55480c1e696a57ee7dd82f70cf5da8932f22a906
saxena100parth <87128985+saxena100parth@users.noreply.github.com>
2023-08-05 21:43:36 +0530

    updated

--- second.js
+++ second.js
@@ -1,37 +1,16 @@
 const IP = localStorage.getItem("ip");
-console.log(IP);
-document.getElementById("ip").innerText = IP;
-let data = {};
-
-function initMap(latt, long) {
-  // Replace YOUR_LATITUDE and YOUR_LONGITUDE with the actual latitude and longitude values
-  var myLatLng = { lat: "", lng: "" };
-  myLatLng.lat = parseFloat(latt);
-  myLatLng.lng = parseFloat(long);
-
-  // Create a map centered at the specified location
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: myLatLng,
-    zoom: 10, // You can adjust the zoom level as needed
-  });
-
-  // Add a marker to the map at the specified location
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: "Location Marker",
-  });
-}
+const filter = document.getElementById("filter");
+const cardsDiv = document.getElementById("cards");
+let postOfficeCard = [];
 
 async function fetchIPInfo() {
   try {
     const response = await fetch(
       `https://ipinfo.io/${IP}?token=53abce53bb77df`
     );
-    data = await response.json();
-
+    const data = await response.json();
     const [latitude, longitude] = data.loc.split(",");
-
+    document.getElementById("ip").innerText = IP;
     document.getElementById("lat").innerText = latitude;
     document.getElementById("long").innerText = longitude;
     document.getElementById("city").innerText = data.city;
@@ -41,41 +20,61 @@ async function fetchIPInfo() {
     document.getElementById("timeZone").innerText = data?.timezone;
     document.getElementById("pincode").innerText = data?.postal;
 
-    // console.log(data);
     initMap(latitude, longitude);
 
     const Zone = await fetch(
       `https://worldtimeapi.org/api/timezone/${data.timezone}`
     );
     const ZoneData = await Zone.json();
-    // console.log(ZoneData);
     document.getElementById("dateTime").innerText = ZoneData.datetime;
 
     const postOffices = await fetch(
       `https://api.postalpincode.in/pincode/${data.postal}`
     );
-    let allPostOffice = await postOffices.json();
+    const allPostOffice = await postOffices.json();
     document.getElementById("allPins").innerText = allPostOffice[0].Message;
-    let postOfficeCard = allPostOffice[0].PostOffice;
-    console.log(postOfficeCard);
-    let cardsDiv = document.getElementById("cards");
-    let filter = document.getElementById("filter");
-    filterValue = filter.value;
-    console.log(filterValue);
-    postOfficeCard.forEach((element) => {
-      let card = document.createElement("div");
-      card.className = "card";
-      card.innerHTML = `<p>Name: <span>${element.Name}</span></p>
-      <p>Branch Type: <span>${element.BranchType}</span></p>
-      <p>Delivery Status: <span>${element.DeliveryStatus} </span></p>
-      <p>District: <span>
-      ${element.District} </span></p>
-      <p>Division: <span>${element.Division} </span></p>`;
-      cardsDiv.appendChild(card);
-    });
+    postOfficeCard = allPostOffice[0].PostOffice;
+    updatePostOfficeCards(postOfficeCard);
   } catch (error) {
     console.error(error);
   }
 }
+
+function initMap(latt, long) {
+  const myLatLng = { lat: parseFloat(latt), lng: parseFloat(long) };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: myLatLng,
+    zoom: 10,
+  });
+  const marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: "Location Marker",
+  });
+}
+
+function updatePostOfficeCards(postOffices) {
+  cardsDiv.innerHTML = ""; // Clear existing cards
+  postOffices.forEach((element) => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `<p>Name: <span>${element.Name}</span></p>
+      <p>Branch Type: <span>${element.BranchType}</span></p>
+      <p>Delivery Status: <span>${element.DeliveryStatus}</span></p>
+      <p>District: <span>${element.District}</span></p>
+      <p>Division: <span>${element.Division}</span></p>`;
+    cardsDiv.appendChild(card);
+  });
+}
+
+function handleChange(event) {
+  const inputValue = event.target.value;
+  const filteredPostOffices = postOfficeCard.filter((element) => {
+    return element.Name.toLowerCase().includes(inputValue.toLowerCase());
+  });
+  updatePostOfficeCards(filteredPostOffices);
+}
+
+filter.addEventListener("input", handleChange);
 
 fetchIPInfo();
